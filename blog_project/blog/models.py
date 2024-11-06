31769d89e997533f2c688ad401e158f36d3fec61
pearsedarcy <pearse.darcy@outlook.com>
2024-11-06 17:55:23 +0100

    Initial commit

---
+++ blog_project/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
